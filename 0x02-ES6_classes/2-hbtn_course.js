2d7730fafddb4bc59812c125817d0714b09483e9
youssef2272002salah <youssef22772002salah@gmail.com>
2024-05-30 22:35:17 +0300

    3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,35 +5,36 @@ export default class HolbertonCourse {
     this.students = students;
   }
 
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
   get name() {
     return this._name;
   }
 
-  set length(length) {
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
-      throw new TypeError('Length must be a number');
+  set name(name) {
+    if ((typeof name !== 'string') && !(name instanceof String)) {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
   }
 
   get length() {
     return this._length;
   }
 
-  set students(students) {
-    if (Array.isArray(students)) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array');
+  set length(length) {
+    if ((typeof length !== 'number') && !(length instanceof Number)) {
+      throw new TypeError('Length must be a number');
     }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
   }
 }
