587e72220e3d335800b86faff23bda197822abe2
youssef2272002salah <youssef22772002salah@gmail.com>
2024-05-30 21:53:00 +0300

    2

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom";
+export default function initializeRooms() {
+   const list = [];
+    list.push(new ClassRoom(19));
+    list.push(new ClassRoom(20));
+    list.push(new ClassRoom(34));
+    return list
+}
