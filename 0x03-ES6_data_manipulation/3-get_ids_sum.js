72fb96354a7f1aa1502039a95a8aa5aa52e96828
youssef2272002salah <youssef22772002salah@gmail.com>
2024-06-04 21:59:05 +0300

    3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
-    return students.reduce;
+    return students.reduce((sum, student) => sum + student.id, 0);
   }
   return 0;
 }
